a5a2882198977d2208a2b885aacb0c8b4ab5ee3b
tszyrowski <tszyrowski@gmail.com>
2020-12-18 18:29:43 +0000

    main: Finished lesson 5

--- first_1/main.c
+++ first_1/main.c
@@ -1,17 +1,17 @@
+#include "lm4f120h5qr.h"
 
 int main(){ 
-
-    *((unsigned int *)0x400fe608U) = 0x20U;
-    *((unsigned int *)0x40025400U) = 0x0EU;
-    *((unsigned int *)0x4002551CU) = 0x0EU;
+    SYSCTL_RCGCGPIO_R = 0x20U;
+    GPIO_PORTF_DIR_R = 0x0EU;
+    GPIO_PORTF_DEN_R = 0x0EU;
     
     while (1) {
-        *((unsigned int *)0x400253FCU) = 0x02U;
-        int counter = 0;
-        while (counter < 1000000){
+        GPIO_PORTF_DATA_R = 0x02U;
+        int volatile counter = 0;
+        while (counter < 500000){
             ++counter;
         }
-        *((unsigned int *)0x400253FCU) = 0x00U;
+        GPIO_PORTF_DATA_R = 0x00U;
         counter = 0;
         while (counter < 1000000){
             ++counter;
