86490afe37490f0f7b229c10cb3e674bcd78c9f1
tszyrowski <tszyrowski@gmail.com>
2020-12-18 19:29:27 +0000

    main: Check through bitwise operations

--- first_1/main.c
+++ first_1/main.c
@@ -1,10 +1,33 @@
 #include "lm4f120h5qr.h"
 
 int main(){ 
+    unsigned int a = 0x5a5a5a5a;
+    unsigned int b = 0xdeadbeef;
+    unsigned int c;
+    unsigned int d;
+    
+    c = a | b;  // OR
+    c = a & b;  // AND
+    c = a ^ b;  //exclusive OR
+    c = ~b;     // NOT
+    d = c;
+    c = b >> 1; // right shift
+    d = c;
+    c = b << 3; // left shift
+    
+    int x = 1024;
+    int y = -1024;
+    int z;
+    
+    z = x >> 3;
+    z = y >> 3;
+    
+    
     SYSCTL_RCGCGPIO_R = 0x20U;
     GPIO_PORTF_DIR_R = 0x0EU;
     GPIO_PORTF_DEN_R = 0x0EU;
     
+    GPIO_PORTF_DATA_R = 0x04U;  // turn the blue LED on
     while (1) {
         GPIO_PORTF_DATA_R = 0x02U;
         int volatile counter = 0;
